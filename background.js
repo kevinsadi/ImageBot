6e32755f3a1778f25345a6db90b4021dc25405aa
PXLante <34428034+PXLante@users.noreply.github.com>
2019-09-13 16:22:09 -0400

    Update background.js
    Simplicity

--- background.js
+++ background.js
@@ -1,10 +1,6 @@
 console.log('Background Running');
 
-chrome.browserAction.onClicked.addListener(buttonClicked);
-
 function buttonClicked(tab) {
-    let msg = {
-        txt: "hello"
     }
-    chrome.tabs.sendMessage(tab.id, msg);
+
 }
