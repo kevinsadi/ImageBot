554e4cc3ad1e77211625db8bf7e9ce0656a89532
mr625 <52550481+mr625@users.noreply.github.com>
2019-09-13 16:31:15 -0400

    Fixed comma

--- content.js
+++ content.js
@@ -7,9 +7,9 @@ chrome.runtime.onMessage.addListener(gotMessage);
 
 const girls = [
 	'https://simg.nicepng.com/png/small/851-8517636_skin-element-http-i-imgur-com-t8thkso-girl.png',
-	'https://i.pinimg.com/236x/7e/f9/68/7ef9680856273163d043024998399f55.jpg'
+	'https://i.pinimg.com/236x/7e/f9/68/7ef9680856273163d043024998399f55.jpg',
 	'https://www.tfwanime.com/wp-content/uploads/2019/03/Shinomiya-kaguya-cat-ears.png',
-    	'https://i.kym-cdn.com/photos/images/original/001/235/259/8cb.jpg'
+	'https://i.kym-cdn.com/photos/images/original/001/235/259/8cb.jpg'
 ];
 
 document.querySelectorAll('img').forEach((img) => {
